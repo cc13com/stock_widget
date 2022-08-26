49bd8c6931f3279a01c69fdd116fc79331bf2554
Carsten Cerny <Carsten.Cerny@cc13.com>
2022-08-26 13:25:29 +0200

    Changed to dark theme and different "real" values

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:stock_widget/provider/darkthemprovider.dart';
+
 import 'package:stock_widget/screens/home_page.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-DarkThemeProvider themeChangeProvider = new DarkThemeProvider();
-
-@override
-void initState() {
-  initState();
-  getCurrentAppTheme();
-}
-
-void getCurrentAppTheme() async {
-  themeChangeProvider.darkTheme =
-      await themeChangeProvider.darkThemePreferences.getTheme();
 }
 
 class MyApp extends StatelessWidget {
@@ -26,20 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) {
-        return themeChangeProvider;
-      },
-      child: Consumer<DarkThemeProvider>(
-        builder: (BuildContext context, value, Widget child) {
-          return MaterialApp(
-            title: 'Flutter Demo',
-            theme: ThemeData.light(),
-            darkTheme: ThemeData.dark(),
-            home: const HomePage(),
-          );
-        },
-      ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(brightness: Brightness.light),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      home: HomePage(),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,25 +1,213 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isDark = false;
+
+  final List<String> longnames = <String>[
+    'NVIDIA CORP',
+    'TARGET CORP. DL-,0833',
+    'TOMRA SYSTEMS ASA NK-,50'
+  ];
+
+  final myNames = List<String>.generate(3, (index) => 'Product $index');
+
+  final List<String> symbol = <String>['NVD.DE', 'DYH.F', 'TMRA.F'];
+  final List<int> amount = <int>[30, 23, 13];
+  final List<double> buyprice = <double>[123.23, 44.23, 998.99];
+  final List<double> stop = <double>[120.01, 50.21, 555.90];
+  final List<double> price = <double>[234.99, 49.00, 600.00];
+  final List<String> lastChangeStop = <String>[
+    '01.08.2022',
+    '13.06.2022',
+    '26.08.2022'
+  ];
+  final List<String> lastUpdate = <String>[
+    '26.08.2022',
+    '25.08.2022',
+    '26.08.2022'
+  ];
+
+  final List<double> changeEuro = <double>[12345.01, 3245.34, 54325.89];
+  final List<double> changePercent = <double>[12.5, 166.4, 1238.2];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Stocks'),
+        actions: [
+          CupertinoSwitch(
+              value: _isDark,
+              onChanged: (v) {
+                setState(() {
+                  _isDark = !_isDark;
+                });
+              })
+        ],
       ),
       body: ListView.builder(
-        itemCount: 10,
+        itemCount: longnames.length,
         itemBuilder: (context, index) {
           //return StockDetails();
-          return const StockDetails1();
+          //return const StockDetails1();
+          return Card(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  child: Row(
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                            text: longnames[index],
+                            style: TextStyle(fontSize: 24),
+                            children: <TextSpan>[
+                              TextSpan(
+                                text: ' (' + symbol[index] + ')',
+                                style: TextStyle(fontSize: 16),
+                              )
+                            ]),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('${amount[index]} / ${buyprice[index]} €'),
+                          Text(
+                            '${stop[index]} € (${lastChangeStop[index]})',
+                            style: TextStyle(color: Colors.green, fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${price[index]} €',
+                            style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '${lastUpdate[index]}',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            '${changeEuro[index]} €',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '${changePercent[index]} %',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          );
         },
+      ),
+    );
+  }
+
+  Widget StockDetails2(BuildContext context, int index) {
+    return Card(
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(5),
+            child: Row(
+              children: [
+                RichText(
+                  text: const TextSpan(
+                      text: 'very long ',
+                      style: TextStyle(fontSize: 24),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: ' (very long Symbol)',
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ]),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text('Amount / buy Price1'),
+                    Text(
+                      'Stop Kurs (last changed)',
+                      style: TextStyle(color: Colors.green, fontSize: 16),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Price',
+                      style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'last Update',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      'Change €',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Change %',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -98,11 +286,11 @@ var stock = Container(
       RichText(
         text: const TextSpan(
             text: 'very long Longname',
-            style: TextStyle(fontSize: 24, color: Colors.black),
+            style: TextStyle(fontSize: 24),
             children: <TextSpan>[
               TextSpan(
                 text: ' (very long Symbol)',
-                style: TextStyle(fontSize: 16, color: Colors.black),
+                style: TextStyle(fontSize: 16),
               )
             ]),
       ),
@@ -130,7 +318,8 @@ final stockDetails = Container(
         children: const [
           Text(
             'Price',
-            style: TextStyle(fontSize: 16, color: Colors.red),
+            style: TextStyle(
+                fontSize: 16, color: Colors.red, fontWeight: FontWeight.bold),
           ),
           Text(
             'last Update',
@@ -143,11 +332,11 @@ final stockDetails = Container(
         children: const [
           Text(
             'Change €',
-            style: TextStyle(fontSize: 16),
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
           Text(
             'Change %',
-            style: TextStyle(fontSize: 16),
+            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
           ),
         ],
       )
